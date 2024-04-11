56cc1a7c65090674a5d985454ef66cc329a64621
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-04-10 22:34:26 -0400

    add middlewares to serve static files, parse request body, configure videos route

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const app = express();
+const videosRoutes = require("./routes/videos");
+
+const requestLogger = (request, response, next) => {
+  console.log(`Method: `, request.method);
+  console.log(`Path: `, request.path);
+  console.log(`Body: `, request.body);
+  console.log(`------------------------------`);
+  next();
+};
+
+app.use(express.json());
+app.use(express.static("./public"));
+app.use(requestLogger);
+app.use("/videos", videosRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
