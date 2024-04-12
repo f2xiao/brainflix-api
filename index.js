da9bdc4ae896b9eafe6d4347a409a027cf3ba264
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-04-12 18:06:13 -0400

    enable cors and requireApiKey middleware function, update the port number

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const videosRoutes = require("./routes/videos");
 
 const requestLogger = (request, response, next) => {
@@ -10,12 +11,27 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+const requireApiKey = (request, response, next) => {
+  const api_key = request.query.api_key;
+  if (api_key) {
+    if (api_key === "32466d85-c74a-4004-8864-0d31ddf91185") {
+      next();
+    }
+  } else {
+    response.status(401).json({
+      message: "require api key",
+    });
+  }
+};
+
+app.use(cors());
 app.use(express.json());
 app.use(express.static("./public"));
 app.use(requestLogger);
+app.use(requireApiKey);
 app.use("/videos", videosRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`server is runnning on port ${PORT}`);
 });
